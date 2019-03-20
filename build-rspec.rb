f31f00a5c803f05543db55fc4a0910ffa7e5db49
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-20 10:23:56 +0000

    added expect class

--- build-rspec.rb
+++ build-rspec.rb
@@ -1,10 +1,12 @@
+class Expect
 
-def expect_to_be_equal(arg1, arg2)
-  if arg1 == arg2
-    return 'Test passes! :)'
-  else
-    return 'Test fails! D:'
+  def to_be_equal(arg1, arg2)
+    if arg1 == arg2
+      return 'Test passes! :)'
+    else
+      return 'Test fails! D:'
+    end
   end
 end
 
-puts expect_to_be_equal(true, false)
+puts Expect.new.to_be_equal(true, true)
