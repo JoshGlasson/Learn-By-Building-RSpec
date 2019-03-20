9d682e788e2b422cb142088f795e04f61cf119a4
Josh Glasson <Josh.Glasson@tesco.com>
2019-03-20 10:51:47 +0000

    Exercise 4. Separate Equal and Expect classes to compare, tests pass

--- build-rspec.rb
+++ build-rspec.rb
@@ -1,18 +1,36 @@
 class Expect
-attr_accessor :arg1
+  attr_accessor :arg1
 
   def initialize(arg1)
     @arg1 = arg1
   end
 
-  def to_equal(arg2)
-    if arg1 == arg2
+  def to(comparison)
+    if comparison.compare(true) == arg1
       puts 'Test passes! :)'
     else
       puts 'Test fails! D:'
     end
   end
+
 end
 
-Expect.new(true).to_equal(true)
-Expect.new(true).to_equal(false)
+class Equal
+  attr_accessor :arg2
+
+  def initialize(arg2)
+    @arg2 = arg2
+  end
+
+  def compare(arg)
+    if arg2 == arg
+      return true
+    else
+      return false
+    end
+  end
+
+end
+
+Expect.new(true).to(Equal.new(true))
+Expect.new(true).to(Equal.new(false))
